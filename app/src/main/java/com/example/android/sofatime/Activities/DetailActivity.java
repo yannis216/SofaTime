a34dc5f23dcddf6e07a2f23b8349ee0c092b44c6
yannis216 <yannisbeuke@googlemail.com>
2018-12-09 11:48:31 +0100

    Star now changing colour when clicked but status is lost when activity is left and reopened

--- app/src/main/java/com/example/android/sofatime/Activities/DetailActivity.java
+++ app/src/main/java/com/example/android/sofatime/Activities/DetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -43,14 +44,34 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         Intent intent = getIntent();
-        Movie detailedMovie = (Movie) intent.getSerializableExtra("requestedMovie");
+        final Movie detailedMovie = (Movie) intent.getSerializableExtra("requestedMovie"); //TODO I do not belieben that final can work here
 
         //getting all the views
         ImageView posterDetailView = findViewById(R.id.iv_poster_detail);
         TextView titleDetailView = findViewById(R.id.tv_title_detail);
         TextView releaseDateDetailView = findViewById(R.id.tv_releasedate_detail);
         TextView ratingDetailView = findViewById(R.id.tv_rating_detail);
-        ImageButton starView = findViewById(R.id.ib_star);
+        final ImageButton starView = findViewById(R.id.ib_star);
+
+        starView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (detailedMovie.isStarred()){
+                    detailedMovie.setStarred(false);
+                    Log.e("StarValue", ""+ detailedMovie.isStarred());
+                }
+                else{
+                    detailedMovie.setStarred(true);
+                    Log.e("StarValue", ""+ detailedMovie.isStarred());
+                }
+                boolean isStarred = detailedMovie.isStarred();
+
+                setImageForStar(isStarred, starView);
+
+            }
+        });
+
+
+
         TextView overviewDetailView = findViewById(R.id.tv_overview_detail);
 
         //getting necessary String Resources
@@ -63,18 +84,20 @@ public class DetailActivity extends AppCompatActivity {
         ratingDetailView.setText(rating_firstPart + detailedMovie.getVoteAverage().toString());
         overviewDetailView.setText(detailedMovie.getOverview());
 
+        //Take care of isStarred on initial load
         boolean isStarred = detailedMovie.isStarred();
-        //Taking care of the Imagebutton
-        if(isStarred){
-            starView.setImageResource(R.drawable.baseline_star_black_18dp);
-            starView.setColorFilter(Color.argb(255,255,255,0));
-        }
+        setImageForStar(isStarred, starView);
+        //TODO Have a look at this when Room is implemented
+
+
 
         //display the poster picture
         String imageUrl = MovieAdapter.BASE_IMG_URL +detailedMovie.getPosterPath();
         Picasso picasso = Picasso.with(posterDetailView.getContext());
         picasso.setLoggingEnabled(true);
         picasso.load(imageUrl).into(posterDetailView);
+
+
 
         loadTrailerData(detailedMovie.getId());
         loadReviewData(detailedMovie.getId());
@@ -151,4 +174,20 @@ public class DetailActivity extends AppCompatActivity {
         mRecyclerViewReviews.setAdapter(mReviewAdapter);
 
     }
+
+    private void setImageForStar(boolean isStarred, ImageButton starView){  //TODO Have a look at this when Room is implemented
+        //Taking care of the Imagebutton
+        if(isStarred){
+            starView.setImageResource(R.drawable.baseline_star_black_18dp);
+            starView.setColorFilter(Color.argb(255,255,255,0));
+        }
+        else{
+            starView.setImageResource((R.drawable.baseline_star_border_black_18dp));
+            starView.setColorFilter(Color.argb(255,255,255,255));
+        }
+    }
+
+
+
+
 }
